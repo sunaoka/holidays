9aef4fa0cf53f82a2076bb3a9b4b76cb5151c5dd
sunaoka <105845+sunaoka@users.noreply.github.com>
2024-08-29 01:14:05 +0000

    Update Holiday

--- src/data/gb.php
+++ src/data/gb.php
@@ -15,27 +15,6 @@ return array (
      'timezone_type' => 3,
      'timezone' => 'UTC',
   )),
-  '2023-01-03' => 
-  \Sunaoka\Holidays\Holiday::__set_state(array(
-     'name' => '2nd January (substitute day) (Scotland)',
-     'date' => '2023-01-03 00:00:00.000000',
-     'timezone_type' => 3,
-     'timezone' => 'UTC',
-  )),
-  '2023-03-01' => 
-  \Sunaoka\Holidays\Holiday::__set_state(array(
-     'name' => 'St. David\'s Day (Wales)',
-     'date' => '2023-03-01 00:00:00.000000',
-     'timezone_type' => 3,
-     'timezone' => 'UTC',
-  )),
-  '2023-03-17' => 
-  \Sunaoka\Holidays\Holiday::__set_state(array(
-     'name' => 'St Patrick\'s Day (Northern Ireland)',
-     'date' => '2023-03-17 00:00:00.000000',
-     'timezone_type' => 3,
-     'timezone' => 'UTC',
-  )),
   '2023-04-07' => 
   \Sunaoka\Holidays\Holiday::__set_state(array(
      'name' => 'Good Friday',
@@ -47,13 +26,6 @@ return array (
   \Sunaoka\Holidays\Holiday::__set_state(array(
      'name' => 'Easter Monday (regional holiday)',
      'date' => '2023-04-10 00:00:00.000000',
-     'timezone_type' => 3,
-     'timezone' => 'UTC',
-  )),
-  '2023-04-23' => 
-  \Sunaoka\Holidays\Holiday::__set_state(array(
-     'name' => 'St. George\'s Day',
-     'date' => '2023-04-23 00:00:00.000000',
      'timezone_type' => 3,
      'timezone' => 'UTC',
   )),
@@ -78,31 +50,10 @@ return array (
      'timezone_type' => 3,
      'timezone' => 'UTC',
   )),
-  '2023-07-12' => 
-  \Sunaoka\Holidays\Holiday::__set_state(array(
-     'name' => 'Battle of the Boyne (Northern Ireland)',
-     'date' => '2023-07-12 00:00:00.000000',
-     'timezone_type' => 3,
-     'timezone' => 'UTC',
-  )),
-  '2023-08-07' => 
-  \Sunaoka\Holidays\Holiday::__set_state(array(
-     'name' => 'Summer Bank Holiday (Scotland)',
-     'date' => '2023-08-07 00:00:00.000000',
-     'timezone_type' => 3,
-     'timezone' => 'UTC',
-  )),
   '2023-08-28' => 
   \Sunaoka\Holidays\Holiday::__set_state(array(
      'name' => 'Summer Bank Holiday (regional holiday)',
      'date' => '2023-08-28 00:00:00.000000',
-     'timezone_type' => 3,
-     'timezone' => 'UTC',
-  )),
-  '2023-11-30' => 
-  \Sunaoka\Holidays\Holiday::__set_state(array(
-     'name' => 'St Andrew\'s Day (Scotland)',
-     'date' => '2023-11-30 00:00:00.000000',
      'timezone_type' => 3,
      'timezone' => 'UTC',
   )),
@@ -127,34 +78,6 @@ return array (
      'timezone_type' => 3,
      'timezone' => 'UTC',
   )),
-  '2024-01-02' => 
-  \Sunaoka\Holidays\Holiday::__set_state(array(
-     'name' => '2nd January (Scotland)',
-     'date' => '2024-01-02 00:00:00.000000',
-     'timezone_type' => 3,
-     'timezone' => 'UTC',
-  )),
-  '2024-03-01' => 
-  \Sunaoka\Holidays\Holiday::__set_state(array(
-     'name' => 'St. David\'s Day (Wales)',
-     'date' => '2024-03-01 00:00:00.000000',
-     'timezone_type' => 3,
-     'timezone' => 'UTC',
-  )),
-  '2024-03-17' => 
-  \Sunaoka\Holidays\Holiday::__set_state(array(
-     'name' => 'St Patrick\'s Day (Northern Ireland)',
-     'date' => '2024-03-17 00:00:00.000000',
-     'timezone_type' => 3,
-     'timezone' => 'UTC',
-  )),
-  '2024-03-18' => 
-  \Sunaoka\Holidays\Holiday::__set_state(array(
-     'name' => 'Day off for St Patrick\'s Day (Northern Ireland)',
-     'date' => '2024-03-18 00:00:00.000000',
-     'timezone_type' => 3,
-     'timezone' => 'UTC',
-  )),
   '2024-03-29' => 
   \Sunaoka\Holidays\Holiday::__set_state(array(
      'name' => 'Good Friday',
@@ -166,13 +89,6 @@ return array (
   \Sunaoka\Holidays\Holiday::__set_state(array(
      'name' => 'Easter Monday (regional holiday)',
      'date' => '2024-04-01 00:00:00.000000',
-     'timezone_type' => 3,
-     'timezone' => 'UTC',
-  )),
-  '2024-04-23' => 
-  \Sunaoka\Holidays\Holiday::__set_state(array(
-     'name' => 'St. George\'s Day',
-     'date' => '2024-04-23 00:00:00.000000',
      'timezone_type' => 3,
      'timezone' => 'UTC',
   )),
@@ -190,38 +106,10 @@ return array (
      'timezone_type' => 3,
      'timezone' => 'UTC',
   )),
-  '2024-07-12' => 
-  \Sunaoka\Holidays\Holiday::__set_state(array(
-     'name' => 'Battle of the Boyne (Northern Ireland)',
-     'date' => '2024-07-12 00:00:00.000000',
-     'timezone_type' => 3,
-     'timezone' => 'UTC',
-  )),
-  '2024-08-05' => 
-  \Sunaoka\Holidays\Holiday::__set_state(array(
-     'name' => 'Summer Bank Holiday (Scotland)',
-     'date' => '2024-08-05 00:00:00.000000',
-     'timezone_type' => 3,
-     'timezone' => 'UTC',
-  )),
   '2024-08-26' => 
   \Sunaoka\Holidays\Holiday::__set_state(array(
      'name' => 'Summer Bank Holiday (regional holiday)',
      'date' => '2024-08-26 00:00:00.000000',
-     'timezone_type' => 3,
-     'timezone' => 'UTC',
-  )),
-  '2024-11-30' => 
-  \Sunaoka\Holidays\Holiday::__set_state(array(
-     'name' => 'St Andrew\'s Day (Scotland)',
-     'date' => '2024-11-30 00:00:00.000000',
-     'timezone_type' => 3,
-     'timezone' => 'UTC',
-  )),
-  '2024-12-02' => 
-  \Sunaoka\Holidays\Holiday::__set_state(array(
-     'name' => 'St Andrew\'s Day observed (Scotland)',
-     'date' => '2024-12-02 00:00:00.000000',
      'timezone_type' => 3,
      'timezone' => 'UTC',
   )),
@@ -246,27 +134,6 @@ return array (
      'timezone_type' => 3,
      'timezone' => 'UTC',
   )),
-  '2025-01-02' => 
-  \Sunaoka\Holidays\Holiday::__set_state(array(
-     'name' => '2nd January (Scotland)',
-     'date' => '2025-01-02 00:00:00.000000',
-     'timezone_type' => 3,
-     'timezone' => 'UTC',
-  )),
-  '2025-03-01' => 
-  \Sunaoka\Holidays\Holiday::__set_state(array(
-     'name' => 'St. David\'s Day (Wales)',
-     'date' => '2025-03-01 00:00:00.000000',
-     'timezone_type' => 3,
-     'timezone' => 'UTC',
-  )),
-  '2025-03-17' => 
-  \Sunaoka\Holidays\Holiday::__set_state(array(
-     'name' => 'St Patrick\'s Day (Northern Ireland)',
-     'date' => '2025-03-17 00:00:00.000000',
-     'timezone_type' => 3,
-     'timezone' => 'UTC',
-  )),
   '2025-04-18' => 
   \Sunaoka\Holidays\Holiday::__set_state(array(
      'name' => 'Good Friday',
@@ -278,13 +145,6 @@ return array (
   \Sunaoka\Holidays\Holiday::__set_state(array(
      'name' => 'Easter Monday (regional holiday)',
      'date' => '2025-04-21 00:00:00.000000',
-     'timezone_type' => 3,
-     'timezone' => 'UTC',
-  )),
-  '2025-04-23' => 
-  \Sunaoka\Holidays\Holiday::__set_state(array(
-     'name' => 'St. George\'s Day',
-     'date' => '2025-04-23 00:00:00.000000',
      'timezone_type' => 3,
      'timezone' => 'UTC',
   )),
@@ -302,45 +162,10 @@ return array (
      'timezone_type' => 3,
      'timezone' => 'UTC',
   )),
-  '2025-07-12' => 
-  \Sunaoka\Holidays\Holiday::__set_state(array(
-     'name' => 'Battle of the Boyne (Northern Ireland)',
-     'date' => '2025-07-12 00:00:00.000000',
-     'timezone_type' => 3,
-     'timezone' => 'UTC',
-  )),
-  '2025-07-14' => 
-  \Sunaoka\Holidays\Holiday::__set_state(array(
-     'name' => 'Battle of the Boyne observed (Northern Ireland)',
-     'date' => '2025-07-14 00:00:00.000000',
-     'timezone_type' => 3,
-     'timezone' => 'UTC',
-  )),
-  '2025-08-04' => 
-  \Sunaoka\Holidays\Holiday::__set_state(array(
-     'name' => 'Summer Bank Holiday (Scotland)',
-     'date' => '2025-08-04 00:00:00.000000',
-     'timezone_type' => 3,
-     'timezone' => 'UTC',
-  )),
   '2025-08-25' => 
   \Sunaoka\Holidays\Holiday::__set_state(array(
      'name' => 'Summer Bank Holiday (regional holiday)',
      'date' => '2025-08-25 00:00:00.000000',
-     'timezone_type' => 3,
-     'timezone' => 'UTC',
-  )),
-  '2025-11-30' => 
-  \Sunaoka\Holidays\Holiday::__set_state(array(
-     'name' => 'St Andrew\'s Day (Scotland)',
-     'date' => '2025-11-30 00:00:00.000000',
-     'timezone_type' => 3,
-     'timezone' => 'UTC',
-  )),
-  '2025-12-01' => 
-  \Sunaoka\Holidays\Holiday::__set_state(array(
-     'name' => 'St Andrew\'s Day observed (Scotland)',
-     'date' => '2025-12-01 00:00:00.000000',
      'timezone_type' => 3,
      'timezone' => 'UTC',
   )),

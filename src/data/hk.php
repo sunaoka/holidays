7989d47c7b0add5839679cd95113250229769f2b
sunaoka <105845+sunaoka@users.noreply.github.com>
2025-06-16 01:45:54 +0000

    Update Holiday

--- src/data/hk.php
+++ src/data/hk.php
@@ -276,15 +276,29 @@ return array (
   )),
   '2026-04-04' => 
   \Sunaoka\Holidays\Holiday::__set_state(array(
-     'name' => 'Tomb Sweeping Day',
+     'name' => 'Holy Saturday',
      'date' => '2026-04-04 00:00:00.000000',
+     'timezone_type' => 3,
+     'timezone' => 'UTC',
+  )),
+  '2026-04-05' => 
+  \Sunaoka\Holidays\Holiday::__set_state(array(
+     'name' => 'Tomb Sweeping Day',
+     'date' => '2026-04-05 00:00:00.000000',
      'timezone_type' => 3,
      'timezone' => 'UTC',
   )),
   '2026-04-06' => 
   \Sunaoka\Holidays\Holiday::__set_state(array(
-     'name' => 'Easter Monday',
+     'name' => 'Day off for Tomb Sweeping Day',
      'date' => '2026-04-06 00:00:00.000000',
+     'timezone_type' => 3,
+     'timezone' => 'UTC',
+  )),
+  '2026-04-07' => 
+  \Sunaoka\Holidays\Holiday::__set_state(array(
+     'name' => 'Day off for Easter Monday',
+     'date' => '2026-04-07 00:00:00.000000',
      'timezone_type' => 3,
      'timezone' => 'UTC',
   )),
